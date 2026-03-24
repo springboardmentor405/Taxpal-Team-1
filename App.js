5a452003a11a9ab3394c952e5bc800c0fe8c6ce4
shwetaverma-hub <artiverma.1214@gmail.com>
2026-03-24 20:50:01 +0530

    Recent Preview page

---
+++ App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import VerifyOTP from "./pages/VerifyOTP";
+import ForgotPassword from "./pages/ForgotPassword";
+import FinancialReport from "./pages/FinancialReport";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/verify" element={<VerifyOTP />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+
+      {/* IMPORTANT: Keep this ABOVE * */}
+      <Route path="/financial-report" element={<FinancialReport />} />
+
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
+}
+
+export default App;
